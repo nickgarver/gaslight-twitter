e94d417f1b4448ae82ee9fac64ef8d5294c83d26
nickgarver <nickgarver5@gmail.com>
2020-01-30 13:33:45 -0500

    follow / unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :tweets
+  has_many :tweets, dependent: :destroy #remove users posts if account deleted
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+  #helper methods
+
+  #follow
+  def follow(other)
+    active_relationships.create(followed_id: other.id)
+  end
+
+  #unfollow
+  def unfollow(other)
+    active_relationships.find_by(followed_id: other.id).destroy
+  end
+
+  def following?(other)
+    following.include?(other)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'registrations' }
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :tweets
+  resources :relationships
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "tweets#index"
 end
